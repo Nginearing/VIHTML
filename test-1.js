febff37b9659f4ba98068ad03a05a6751880f16e
Nginearing <142851004+Nginearing@users.noreply.github.com>
2025-09-27 18:38:28 +0100

    Create test-1.js

---
+++ test-1.js
@@ -0,0 +1,84 @@
+const voidElements = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'link',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr'
+];
+
+const htmlConfig = {
+  tags: {
+    'atama': 'head',
+    'presmi': 'button',
+    'namae': 'title',
+    'vikti': 'meta',
+    'glavna': 'body',
+    'oba1': 'h1',
+    'oba2': 'h2',
+    'oba3': 'h3',
+    'baksu': 'div',
+    'riso': 'img',
+    'pr': 'br'
+  },
+  attributes: {
+    'kran': 'src',
+    'alfakaban': 'charset',
+    'namae': 'name',
+    'shiruzma': 'content',
+    'impla': 'alt'
+  }
+};
+
+function convertCustomHTML(config) {
+  document.querySelectorAll('*').forEach(el => {
+    const tagName = el.tagName.toLowerCase();
+
+    if (config.tags[tagName]) {
+      const newTagName = config.tags[tagName];
+      const newEl = document.createElement(newTagName);
+
+      for (let attr of el.attributes) {
+        const mappedAttr = config.attributes[attr.name];
+        if (typeof mappedAttr === 'string') {
+          newEl.setAttribute(mappedAttr, attr.value);
+        } else if (typeof mappedAttr === 'function') {
+          mappedAttr(newEl, attr.value);
+        } else {
+          newEl.setAttribute(attr.name, attr.value);
+        }
+      }
+
+      const parent = el.parentNode;
+
+      if (voidElements.includes(newTagName)) {
+        parent.insertBefore(newEl, el);
+
+        while (el.firstChild) {
+          parent.insertBefore(el.firstChild, newEl.nextSibling);
+        }
+
+        el.remove();
+      } else {
+        while (el.firstChild) {
+          newEl.appendChild(el.firstChild);
+        }
+        el.replaceWith(newEl);
+      }
+
+      if (newTagName === 'title') {
+        document.title = newEl.textContent;
+      }
+    }
+  });
+}
+
+convertCustomHTML(htmlConfig);
